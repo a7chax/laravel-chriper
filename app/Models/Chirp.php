b12dcb4ab9f08c338b92e36e1a3adcc3a06a4300
arungicahaya <akuarung83@gmail.com>
2023-06-02 21:51:25 +0700

    sending notifications when created chirp

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Events\ChirpCreated;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,10 @@ class Chirp extends Model
 
     protected $attributes =[
         'arung' => "arung"
+    ];
+
+    protected $dispatchesEvents = [
+        'created' => ChirpCreated::class,
     ];
 
     protected $fillable = [
